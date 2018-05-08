44132da2588c95b85d057cb1785070122a4e3fd9
Rolf Madsen <rolfrm@gmail.com>
2018-05-08 22:46:27 +0200

    work on debugging server

--- src/datastream_server.c
+++ src/datastream_server.c
@@ -11,10 +11,10 @@
 #define PORT 8888
 
 typedef enum{
-  DISABLED = 0,
-  WAITING_FOR_ENABLED,
-  ENABLED,
-  WAITING_FOR_DISABLED
+  ACTIVITY_DISABLED = 0,
+  ACTIVITY_WAITING_FOR_ENABLED = 1,
+  ACTIVITY_ENABLED = 2,
+  ACTIVITY_WAITING_FOR_DISABLED = 3
 }activity_state;
 
 struct _message_list;
@@ -41,58 +41,14 @@ typedef struct{
 
 static data_stream weblog = {.name = "dlog web"};
 
-// [index] -> load the page file
-// get_streams -> get the list of streams
-// enable_streams -> enables a set of streams
-// listen -> Gets buffered activitys.
-static int
-answer_to_connection (void *cls, struct MHD_Connection *connection,
-                      const char *url, const char *method,
-                      const char *version, const char *upload_data,
-                      size_t *upload_data_size, void **con_cls)
-{
-  
-  UNUSED(method);
-  UNUSED(version);
-  UNUSED(upload_data);
-  UNUSED(upload_data_size);
-  UNUSED(con_cls);
-  dmsg(weblog, "URL: %s\n", url);
-  size_t count = 4;
-  char * message = alloc(count);
-  char * msg = message;
-
-  void fmt(const char * _fmt, ...){
-
-    va_list args;
-    va_start (args, _fmt);
-  retry_printf:;
-    size_t n = msg - message;
-    size_t max_write = count - n;
-    size_t cnt;
-    if(max_write <= 1){
-      cnt = strlen(_fmt);
-    }else{
-      cnt = vsnprintf(msg, max_write, _fmt, args);
-    }
-    if(cnt > 0){
-      //logd("cnt: %i %i %i\n", cnt, n, count);
-      if(cnt >= max_write){
-	message = ralloc(message, (count = (count + cnt) * 2));
-	msg = message + n;
-	goto retry_printf;
-      }
-      msg += cnt;
-    }
-    va_end(args);
-  }
-
-
+static void get_system_update(listen_ctx * ctx, void (*fmt)(const char * _fmt, ...)){
   fmt("<html><body>");
-  listen_ctx * ctx = cls;
   for(size_t i = 0; i< ctx->activity_count; i++){
-    dlog(weblog, ctx->activitys[i], ctx->activity_length[i]);  
-    fmt("<input type=\"checkbox\">%s</input>", ctx->activitys[i]);    
+    dlog(weblog, ctx->activitys[i], ctx->activity_length[i]);
+    bool active = false;
+    if(ctx->activity_state[i] == ACTIVITY_ENABLED || ctx->activity_state[i] == ACTIVITY_WAITING_FOR_ENABLED)
+      active = true;
+    fmt("<input type=\"checkbox\" %s>%s</input>", active ? "checked" : "", ctx->activitys[i]);    
   }
   fmt("<h3>Messages:</h3>");  
   if(ctx->messages != NULL){
@@ -116,10 +72,87 @@ answer_to_connection (void *cls, struct MHD_Connection *connection,
   }
   
   fmt("</body></html>");
-  size_t len = msg - message;
-  dmsg(weblog, "Response of : %i bytes\n", len);  
-  var response =
-     MHD_create_response_from_buffer (len, message,MHD_RESPMEM_MUST_FREE);
+}
+
+static void get_activities(listen_ctx * ctx, void (*fmt)(const char * _fmt, ...)){
+  for(size_t i = 0; i< ctx->activity_count; i++){
+    dlog(weblog, ctx->activitys[i], ctx->activity_length[i]);
+    bool active = false;
+    if(ctx->activity_state[i] == ACTIVITY_ENABLED || ctx->activity_state[i] == ACTIVITY_WAITING_FOR_ENABLED)
+      active = true;
+    fmt("%s: %i\n", ctx->activitys[i], active ? "checked" : "");    
+  }
+}
+
+static void read_index_file(listen_ctx * ctx, void (*fmt)(const char * _fmt, ...)){
+  UNUSED(ctx);
+  var filecontent = read_file_to_string("page_header.html");
+  fmt("%s", filecontent);
+  dealloc(filecontent);
+}
+
+// [index] -> load the page file
+// get_streams -> get the list of streams
+// enable_streams -> enables a set of streams
+// listen -> Gets buffered activitys.
+static int
+answer_to_connection (void *cls, struct MHD_Connection *connection,
+                      const char *url, const char *method,
+                      const char *version, const char *upload_data,
+                      size_t *upload_data_size, void **con_cls)
+{
+
+
+  
+  UNUSED(method);
+  UNUSED(version);
+  UNUSED(upload_data);
+  UNUSED(upload_data_size);
+  UNUSED(con_cls);
+
+  dmsg(weblog, "URL: '%s'\n", url);
+  
+  size_t count = 4;
+  char * message = alloc(count);
+  char * msg = message;
+  listen_ctx * ctx = cls;
+  
+  void fmt(const char * _fmt, ...){
+
+  retry_printf:;
+    size_t n = msg - message;
+    size_t max_write = count - n;
+    size_t cnt;
+    if(max_write <= 1){
+      cnt = strlen(_fmt);
+      message = ralloc(message, (count = (count + cnt) * 2));
+      msg = message + n;
+      goto retry_printf;
+    }else{
+      va_list args;
+      va_start (args, _fmt);
+      cnt = vsnprintf(msg, max_write - 1, _fmt, args);
+      va_end(args);
+    }
+    if(cnt > 0){
+      if(cnt >= max_write){
+	message = ralloc(message, (count = (count + cnt) * 2));
+	msg = message + n;
+	goto retry_printf;
+      }
+      msg += cnt;
+    }
+  }
+
+  if(strcmp("/activities", url) == 0){
+    get_activities(ctx, fmt);
+  }else if(strcmp("/update", url) == 0){
+    get_system_update(ctx, fmt);
+  }else{
+    read_index_file(ctx, fmt);
+  }
+
+  var response = MHD_create_response_from_buffer (count, message,MHD_RESPMEM_MUST_FREE);
    
   int ret =  MHD_queue_response (connection, MHD_HTTP_OK,response);
   MHD_destroy_response(response);
@@ -144,7 +177,7 @@ static void activity_update(const data_stream * s, const void * data, size_t len
   ctx->activity_state = ralloc(ctx->activity_state, sizeof(ctx->activity_state[0]) * ctx->activity_count);
   ctx->activitys[ctx->activity_count - 1]= fmtstr("%s", s->name);// iron_clone(s->name, strlen(s->name) + 1);
   ctx->activity_length[ctx->activity_count - 1] = strlen(s->name) + 1;
-  ctx->activity_state[ctx->activity_count - 1] = ENABLED;
+  ctx->activity_state[ctx->activity_count - 1] = ACTIVITY_ENABLED;
 
   iron_mutex_unlock(ctx->lock);
   data_stream_listen(ctx->data_listener, (data_stream *) s);
